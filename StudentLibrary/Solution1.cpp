c82a62cc5482cab86c3ee333f1da1fdd92a85ead
unknown <ycaillot@gamingcampus.local>
2024-01-19 13:37:40 +0100

    exercice 1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -6,7 +6,66 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+	switch (_from)
+	{
+	case TemperatureUnits::CELSIUS:
+		switch (_to)
+		{
+		case TemperatureUnits::CELSIUS:
+			return _value;
+			break;
+		case TemperatureUnits::FAHRENHEIT:
+			return ((9.f / 5.f * _value) + 32.f);
+			break;
+		case TemperatureUnits::KELVIN:
+			return (_value + 273.15);
+			break;
+		default:
+			break;
+		}
+		break;
+
+
+	case TemperatureUnits::FAHRENHEIT:
+		switch (_to)
+		{
+		case TemperatureUnits::CELSIUS:
+			return (5.f / 9.f * (_value - 32.f));
+			break;
+		case TemperatureUnits::FAHRENHEIT:
+			return _value;
+			break;
+		case TemperatureUnits::KELVIN:
+			return (5.f / 9.f * (_value - 32.f) + 273.15);
+			break;
+		default:
+			break;
+		}
+		break;
+
+
+	case TemperatureUnits::KELVIN:
+		switch (_to)
+		{
+		case TemperatureUnits::CELSIUS:
+			return (_value - 273.15);
+			break;
+		case TemperatureUnits::FAHRENHEIT:
+			return (9.f / 5.f * (_value - 273.15) + 32.f);
+			break;
+		case TemperatureUnits::KELVIN:
+			return _value;
+			break;
+		default:
+			break;
+		}
+		break;
+
+
+	default:
+		break;
+
+	}
 }
 
 #endif
